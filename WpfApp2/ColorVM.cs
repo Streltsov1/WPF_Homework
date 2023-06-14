e5ae43d06af679bac8d6adf0a66e148e898396cf
Oleg Streltsov <olegstrelsov2003@gmail.com>
2023-06-14 21:47:41 +0300

    Color viewer

---
+++ WpfApp2/ColorVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using PropertyChanged;
+
+namespace WpfApp2
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ColorVM
+    {
+        [DependsOn("Alpha", "Red", "Green", "Blue")]
+        public Color Color => Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue);
+        public int Alpha { get; set; }
+        public int Red { get; set; }
+
+        public int Blue { get; set; }
+        public int Green { get; set; }
+        [DependsOn("Color", "Alpha", "Red", "Green", "Blue")]
+        public string ShortInfo => ToString();
+        public ColorVM()
+        {
+
+        }
+        public object Clone()
+        {
+            var copy = (ColorVM)this.MemberwiseClone();
+
+            copy.Alpha = (int)this.Alpha;
+            copy.Red = (int)this.Red;
+            copy.Blue = (int)this.Blue;
+            copy.Green = (int)this.Green;
+
+            return copy;
+        }
+        public override string ToString()
+        {
+            return Color.ToString();
+        }
+    }
+}
---
+++ WpfApp2/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        private ColorVM selectedColor;
+        public ColorVM SelectedColor
+        {
+            get => selectedColor;
+            set
+            {
+                if (value == null)
+                    selectedColor = new();
+                else
+                {
+                    selectedColor = value;
+                }
+
+            }
+        }
+        public int? Index { get; set; }
+        private ObservableCollection<ColorVM> colors = new ObservableCollection<ColorVM>();
+        public IEnumerable<ColorVM> Colors => colors;
+        private readonly RelayCommand addColor;
+        private readonly RelayCommand removeColor;
+        public ViewModel()
+        {
+            addColor = new((o) => Add(), (o) => !colors.Contains(SelectedColor));
+            removeColor = new((o) => Delete(), (o) => Index != null);
+            SelectedColor = new();
+        }
+        public ICommand AddCmd => addColor;
+        public ICommand RemoveCmd => removeColor;
+        public void Delete()
+        {
+            colors.Remove(SelectedColor);
+            Index = null;
+        }
+        public void Add()
+        {
+            colors.Add((ColorVM)SelectedColor.Clone());
+        }
+    }
+}
